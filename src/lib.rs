e00e8e48af01ce81c15faec52464841cef17dca9
Michael Kirkland <mak.kirkland@proton.me>
2025-01-11 19:49:15 +0100

    Introduce a GraphValidator and its methods
    - We can now validate the structure of a graph by creating a validator
    - The validator implements various methods that check that the
      structure of the graph obeys the rules of birdtracks
    - Add unit tests for validation methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 struct Node {
@@ -126,6 +128,120 @@ impl Graph {
             edge.from
         }
     }
+
+    fn validate(&self) -> Vec<Result<(), ValidationError>> {
+        let validator = GraphValidator::new(self);
+        validator.validate()
+    }
+}
+
+#[derive(Debug)]
+struct ValidationError {
+    kind: ValidationErrorKind,
+    message: String,
+}
+
+#[derive(Debug)]
+enum ValidationErrorKind {
+    TooManyEdges,
+    TwoSquigglyVertex,
+    StraightToSquiggly,
+    DanglingNode,
+}
+
+use ValidationErrorKind::*;
+
+impl ValidationError {
+    fn new(kind: ValidationErrorKind) -> Self {
+        ValidationError {
+            message: match kind {
+                TooManyEdges => "A node can have at most 3 edges.".to_owned(),
+                TwoSquigglyVertex => "A node can have at most 1 squiggly edge.".to_owned(),
+                StraightToSquiggly => {
+                    "A node with only 2 edges must ensure that the edges are the same kind."
+                        .to_owned()
+                }
+                DanglingNode => "A node must have an edge.".to_owned(),
+            },
+            kind,
+        }
+    }
+
+    fn kind(&self) -> &ValidationErrorKind {
+        &self.kind
+    }
+}
+
+impl fmt::Display for ValidationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for ValidationError {}
+
+// Validator for the graph structure
+struct GraphValidator<'a> {
+    graph: &'a Graph, // We borrow the Graph, so tie the lifetimes
+}
+
+impl<'a> GraphValidator<'a> {
+    fn new(graph: &'a Graph) -> Self {
+        GraphValidator { graph }
+    }
+
+    // Validate the entire graph
+    fn validate(&self) -> Vec<Result<(), ValidationError>> {
+        let mut results = vec![];
+        for node in self.graph.nodes.values() {
+            results.push(self.validate_node(node));
+        }
+        results
+    }
+
+    // Validate a specific node in the graph
+    fn validate_node(&self, node: &Node) -> Result<(), ValidationError> {
+        match node.edges.len() {
+            0 => Err(ValidationError::new(DanglingNode)),
+            1 => Ok(()),
+            2 => {
+                if self.is_straight_to_squiggly(node) {
+                    Err(ValidationError::new(StraightToSquiggly))
+                } else {
+                    Ok(())
+                }
+            }
+            3 => {
+                if self.is_1_straight_2_squiggly(node) {
+                    Err(ValidationError::new(TwoSquigglyVertex))
+                } else {
+                    Ok(())
+                }
+            }
+            _ => Err(ValidationError::new(TooManyEdges)),
+        }
+    }
+
+    // Check if a node has exactly two edges, one straight and one squiggly
+    fn is_straight_to_squiggly(&self, node: &Node) -> bool {
+        let edges = &node.edges;
+        let edge1 = self.graph.edges.get(&edges[0]);
+        let edge2 = self.graph.edges.get(&edges[1]);
+        edge1.unwrap().kind != edge2.unwrap().kind
+    }
+
+    // Check if a node has exactly 1 straight edge and 2 squiggly edges
+    fn is_1_straight_2_squiggly(&self, node: &Node) -> bool {
+        let edges = &node.edges;
+        let edge1 = self.graph.edges.get(&edges[0]).unwrap().kind;
+        let edge2 = self.graph.edges.get(&edges[1]).unwrap().kind;
+        let edge3 = self.graph.edges.get(&edges[2]).unwrap().kind;
+        let count = [edge1, edge2, edge3]
+            .iter()
+            .filter(|&&e| e == EdgeKind::Squiggly)
+            .count();
+        count == 2
+    }
 }
 
 #[cfg(test)]
@@ -172,5 +288,29 @@ mod tests {
         println!("After: {:#?}", graph);
         assert!(graph.nodes.len() == 2);
         assert!(graph.edges.len() == 1);
+    }
+
+    #[test]
+    fn test_validate_straight_to_squiggly() {
+        let mut graph = Graph::new();
+        let node1 = graph.add_node();
+        let node2 = graph.add_node();
+        let node3 = graph.add_node();
+        graph.add_edge(node1, node2, EdgeKind::Straight);
+        graph.add_edge(node2, node3, EdgeKind::Squiggly);
+        assert!(graph.validate().iter().any(|result| result.is_err()));
+    }
+
+    #[test]
+    fn test_validate_1_straight_2_squiggly() {
+        let mut graph = Graph::new();
+        let node1 = graph.add_node();
+        let node2 = graph.add_node();
+        let node3 = graph.add_node();
+        let node4 = graph.add_node();
+        graph.add_edge(node1, node2, EdgeKind::Straight);
+        graph.add_edge(node1, node3, EdgeKind::Squiggly);
+        graph.add_edge(node1, node4, EdgeKind::Squiggly);
+        assert!(graph.validate().iter().any(|result| result.is_err()));
     }
 }
